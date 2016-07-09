7bcbbc215504d5037ec6ce97d0906fa229a28f2a
Shane Boyar <shane.boyar@gmail.com>
2016-07-09 14:49:50 -0400

    Create new post form and let users delete their own posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,33 @@
 class PostsController < ApplicationController
+
+	def new
+		@post = current_user.posts.build
+		respond_to do |format|               
+			format.js
+		end
+	end 
+
+	def create
+		@post = current_user.posts.build(post_params)
+		if @post.save
+			redirect_to user_path(current_user)
+		else
+			render root_path
+		end
+	end
+
+	def destroy
+		@post = current_user.posts.find_by(id: params[:id])
+    	@post.destroy
+    	redirect_to request.referrer || root_url
+  end
+
+
+
+	private
+
+    def post_params
+      params.require(:post).permit(:content)
+    end
+
 end
