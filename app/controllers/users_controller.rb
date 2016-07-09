2c21fb560337beaa7f87cc52517a1498dae33b00
Shane Boyar <shane.boyar@gmail.com>
2016-07-08 23:26:01 -0400

    Build logic so people can only edit their own profiles.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+	before_action :be_the_right_user, only: [:edit]
+
 
 	def new
 		@user = User.new
@@ -22,10 +24,21 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 	end
 
+	def edit
+    	@user = User.find(params[:id])
+  	end
+
 	private
 
 	def user_params
 		params.require(:user).permit(:first_name, :last_name, :email, :password)
 	end
+
+	def be_the_right_user
+		@user = User.find(params[:id])
+		unless current_user.id == @user.id
+			redirect_to root_path
+		end
+	end 
 
 end
