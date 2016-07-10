7670ffbb7895e5c7de6c121f8a0fb80f85839c96
Shane Boyar <shane.boyar@gmail.com>
2016-07-10 00:07:33 -0400

    Hotfix for redirecting back to homepage after failed login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
 	    session[:user_id] = @user.id
 	    redirect_to user_path(@user)
 	  else
-	    redirect_to '/login'
+	    redirect_to '/'
 	  end 
 	end
 
