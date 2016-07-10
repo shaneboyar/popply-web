59af44f8f74785b1826381460ab38ef0559bd8d7
Shane Boyar <shane.boyar@gmail.com>
2016-07-10 00:10:22 -0400

    Hotfix for redirecting to profile page after successful login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
 		@user = User.new(user_params)
 		if @user.save 
     		session[:user_id] = @user.id 
-    		redirect_to users_path(@user) 
+    		redirect_to user_path(@user) 
   		else 
     		redirect_to '/' 
   		end 
