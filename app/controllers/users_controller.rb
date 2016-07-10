f2a573243a434f694eb566440a4531b8a7091257
Shane Boyar <shane.boyar@gmail.com>
2016-07-10 15:13:37 -0400

    Clean up Models and Views and Add League Partial to Profile Page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
 
 	def show
 		@user = User.find(params[:id])
-		@posts = @user.posts.order(created_at: :desc).paginate(page: params[:page]).per_page(10)
+		@leagues = @user.leagues.order(created_at: :desc).paginate(page: params[:page]).per_page(10)
 	end
 
 	def edit
