422e6527e8f361ebf39aae5647016551a56da4c4
Shane Boyar <shane.boyar@gmail.com>
2016-07-15 08:03:34 -0400

    Add rspec tests helpers and reporters

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,12 +2,23 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'capybara/rails'
+require "minitest/reporters"
+Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+
+  # Logs in a test user.
+  def log_in_as(user, options = {})
+    password    = options[:password]    || 'password'
+    post login_path, session: { email:       user.email,
+                                password:    password}
+  end
+
+
 end
 
 class ActionDispatch::IntegrationTest
