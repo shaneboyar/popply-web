1f074d3ae51b309cc143dc97f6c24062a88ae2c2
Shane Boyar <shane.boyar@gmail.com>
2016-07-22 10:12:17 -0400

    Limit seed data to development

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,71 +5,76 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-10.times do 
-	first_name = Faker::Name.first_name
-	last_name = Faker::Name.last_name
-	email = Faker::Internet.free_email(first_name)
-	password = Faker::Internet.password
-	user = User.new(first_name: first_name, last_name: last_name, email: email, password: password)
-	user.save
-end
 
-first_name = "Shane"
-last_name = "Boyar"
-email = "shane.boyar@gmail.com"
-password = "password"
-user = User.new(first_name: first_name, last_name: last_name, email: email, password: password, admin: true)
-user.save
+if Rails.env.development?
 
-title = "The Bachelor"
-start = Faker::Time.forward(23, :morning)
-show = Show.new(title: title, start: start)
-show.save
-
-26.times do 
-	name = Faker::Name.name
-	image = Faker::Avatar.image
-	bio = Faker::Hipster.paragraph
-	age = Faker::Number.between(23, 35)
-	show = Show.find(1)
-	occupation = Faker::Company.profession
-	contestant = Contestant.new(name: name, image: image, bio: bio, age: age, occupation: occupation, show_id: show.id)
-	contestant.save
-end
-
-show = Show.find(1)
-
-(1..10).each do |n|
-show.show_weeks.create(week: n, picklimit: (11-n))
-end
-
-10.times do |n|
-	(n+1).times do |x|
-		contestant = Contestant.find(x+1)
-		show.keys.create(contestant: contestant, week: 10-n)
+	10.times do 
+		first_name = Faker::Name.first_name
+		last_name = Faker::Name.last_name
+		email = Faker::Internet.free_email(first_name)
+		password = Faker::Internet.password
+		user = User.new(first_name: first_name, last_name: last_name, email: email, password: password)
+		user.save
 	end
-end
 
-3.times do 
-	user_id = Faker::Number.between(1, 10)
-	user = User.find(user_id)
-	title = Faker::Superhero.name + " League"
+	first_name = "Shane"
+	last_name = "Boyar"
+	email = "shane.boyar@gmail.com"
+	password = "password"
+	user = User.new(first_name: first_name, last_name: last_name, email: email, password: password, admin: true)
+	user.save
+
+	title = "The Bachelor"
+	start = Faker::Time.forward(23, :morning)
+	show = Show.new(title: title, start: start)
+	show.save
+
+	26.times do 
+		name = Faker::Name.name
+		image = Faker::Avatar.image
+		bio = Faker::Hipster.paragraph
+		age = Faker::Number.between(23, 35)
+		show = Show.find(1)
+		occupation = Faker::Company.profession
+		contestant = Contestant.new(name: name, image: image, bio: bio, age: age, occupation: occupation, show_id: show.id)
+		contestant.save
+	end
+
 	show = Show.find(1)
-	params = { owner_id: user.id, title: title, show_id: show.id}
-	user.groups.create(params)
-end
 
-users = User.all
-groups = Group.all
-owners = []
-groups.each do |group|
-	owner = User.find(group.owner_id)
-	owners.push(owner)
-end
+	(1..10).each do |n|
+	show.show_weeks.create(week: n, picklimit: (11-n))
+	end
 
-non_owners = users - owners
-non_owners.each do |user|
-	group_id = Faker::Number.between(1, 3)
-	group = Group.find(group_id)
-	group.members.push(user)
+	10.times do |n|
+		(n+1).times do |x|
+			contestant = Contestant.find(x+1)
+			show.keys.create(contestant: contestant, week: 10-n)
+		end
+	end
+
+	3.times do 
+		user_id = Faker::Number.between(1, 10)
+		user = User.find(user_id)
+		title = Faker::Superhero.name + " League"
+		show = Show.find(1)
+		params = { owner_id: user.id, title: title, show_id: show.id}
+		user.groups.create(params)
+	end
+
+	users = User.all
+	groups = Group.all
+	owners = []
+	groups.each do |group|
+		owner = User.find(group.owner_id)
+		owners.push(owner)
+	end
+
+	non_owners = users - owners
+	non_owners.each do |user|
+		group_id = Faker::Number.between(1, 3)
+		group = Group.find(group_id)
+		group.members.push(user)
+	end
+
 end
