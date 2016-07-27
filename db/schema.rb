806310e258a4605f5a2795fd61b4574bac081a9a
Shane Boyar <shane.boyar@gmail.com>
2016-07-27 13:35:36 -0400

    Add title to Posts schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160726183432) do
+ActiveRecord::Schema.define(version: 20160727173338) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "post_id"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 20160726183432) do
     t.text     "content"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "title"
   end
 
   add_index "posts", ["group_id"], name: "index_posts_on_group_id"
